9853e7000a490b40423cd8428765070aeaff4fd7
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2019-11-25 18:32:36 -0300

    Updates

---
+++ precache-manifest.6de95e715a431572f2c49c8a4f7f6525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5021b329638d2b1ef12448756023fe36",
+    "url": "/react-player/index.html"
+  },
+  {
+    "revision": "4e0afcb270246ec3c263",
+    "url": "/react-player/static/css/main.a6e90530.chunk.css"
+  },
+  {
+    "revision": "20b2fbd1bd69bec62434",
+    "url": "/react-player/static/js/2.315a33e6.chunk.js"
+  },
+  {
+    "revision": "4e0afcb270246ec3c263",
+    "url": "/react-player/static/js/main.3649da5a.chunk.js"
+  },
+  {
+    "revision": "13b9b435b173bdf2e877",
+    "url": "/react-player/static/js/runtime-main.3ccbea79.js"
+  }
+]);
